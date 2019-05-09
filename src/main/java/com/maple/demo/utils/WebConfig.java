76451b0ae1aa8553fb9737992463eee4ca27d311
NiklausLl <376005455@qq.com>
2019-05-09 10:22:27 +0800

    Merge branch 'master' of https://github.com/hack-feng/maple
    # Conflicts:
    #	pom.xml
    #	src/main/resources/application.properties

---
+++ src/main/java/com/maple/demo/utils/WebConfig.java
@@ -0,0 +1,27 @@
+package com.maple.demo.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //用户登录存放的session
+    public static final String LOGIN_USER = "LOGIN_USER";
+
+    /**
+     * 配置jsp页面
+     * @return
+     */
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
